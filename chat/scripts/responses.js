3580bfd95e53f5606de32783ea6075558de8901e
dimusz <dina.lucian@yahoo.com>
2022-07-20 14:56:45 +0300

    add

---
+++ chat/scripts/responses.js
@@ -0,0 +1,10 @@
+function getBotResponse(input) {
+  if (input == "Buna ziua") {
+    return "Buna ziua!";
+  } else if (input == "La revedere") {
+    return "La revedere!";
+  } 
+  else {
+    return "Imediat veti fi preluat de un consultant. Va multumim!";
+  }
+}
